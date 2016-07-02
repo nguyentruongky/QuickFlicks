689484de02e3302e13a425186764c65b18dbab88
Nguyen Truong Ky <nguyentruongky3390@yahoo.com>
2016-07-02 19:55:11 +0700

    Implement movie list view

---
+++ QuickFlicks/MovieListDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  MovieListDelegate.swift
+//  QuickFlicks
+//
+//  Created by Ky Nguyen on 7/2/16.
+//  Copyright © 2016 Ky Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MovieListDelegate: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
