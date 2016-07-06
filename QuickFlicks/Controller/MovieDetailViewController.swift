5cb1ed5423ee4a87ed7a35d1e7055a960cd45d4d
Dang Quoc Huy <huysamdua@yahoo.com>
2016-07-06 14:04:08 +0700

    Detail View with Autolayout by Stevia

--- QuickFlicks/Controller/MovieDetailViewController.swift
+++ QuickFlicks/Controller/MovieDetailViewController.swift
@@ -9,19 +9,108 @@
 import UIKit
 import Alamofire
 import AlamofireImage
+import SteviaLayout
 
-class MovieDetailViewController: UIViewController, UIScrollViewDelegate {
+class MovieDetailView: UIView {
+    
+    var posterImageView = UIImageView()
+    var contentView = UIView()
+    var titleLabel = UILabel()
+    var dateLabel = UILabel()
+    var rateLabel = UILabel()
+    var timeLabel = UILabel()
+    var overviewLabel = UILabel()
+    
+    convenience init() {
+        self.init(frame: CGRectZero)
+        
+        render()
+    }
+    
+    func render() {
+        
+        backgroundColor = .whiteColor()
+        
+        // Setup UI, need to clean up later
+        // === BEGIN ===
+        posterImageView.contentMode = .ScaleAspectFill
+        
+        contentView.backgroundColor = UIColor.blackColor()
+        contentView.alpha = 0.8
+        
+        titleLabel.textColor = UIColor.whiteColor()
+        titleLabel.numberOfLines = 0
+        titleLabel.font = UIFont.boldSystemFontOfSize(17)
+        dateLabel.textColor = UIColor.whiteColor()
+        dateLabel.font = dateLabel.font.fontWithSize(13)
+        rateLabel.textColor = UIColor.whiteColor()
+        rateLabel.font = rateLabel.font.fontWithSize(13)
+        timeLabel.textColor = UIColor.whiteColor()
+        timeLabel.font = timeLabel.font.fontWithSize(13)
+        overviewLabel.textColor = UIColor.whiteColor()
+        overviewLabel.font = overviewLabel.font.fontWithSize(13)
+        overviewLabel.numberOfLines = 0
+        // === END ===
+        
+        sv(
+            posterImageView.sv(
+                contentView.sv(
+                    titleLabel,
+                    dateLabel,
+                    rateLabel,
+                    timeLabel,
+                    overviewLabel
+                )
+            )
+        )
+        
+        layout(
+            0,
+            |posterImageView|,
+            0
+        )
+        
+        posterImageView.layout(
+            |-32-contentView.height(150)-32-|,
+            0
+        )
+        
+        contentView.layout(
+            0,
+            |-8-titleLabel-8-|,
+            5,
+            |-8-dateLabel-8-|,
+            20,
+            |-8-rateLabel-timeLabel-8-|,
+            5,
+            |-8-overviewLabel-8-|,
+            0
+        )
+    }
+    
+    func fieldStyle(f:UITextField) {
+        f.borderStyle = .RoundedRect
+        f.font = UIFont(name: "HelveticaNeue-Light", size: 26)
+        f.returnKeyType = .Next
+    }
+    
+    func passwordFieldStyle(f:UITextField) {
+        f.secureTextEntry = true
+        f.returnKeyType = .Done
+    }
+    
+    func buttonSytle(b:UIButton) {
+        b.backgroundColor = .lightGrayColor()
+    }
+    
+    func loginTapped() {
+        //Do something
+    }
+}
 
-//    @IBOutlet weak var scrollView: UIScrollView!
-//    @IBOutlet weak var posterImageView: UIImageView!
-//    @IBOutlet weak var titleLabel: UILabel!
-//    @IBOutlet weak var dateLabel: UILabel!
-//    @IBOutlet weak var rateLabel: UILabel!
-//    @IBOutlet weak var timeLabel: UILabel!
-//    @IBOutlet weak var overviewLabel: UILabel!
-//    @IBOutlet weak var overviewScrollView: UIScrollView!
-//    @IBOutlet weak var contentViewHeight: NSLayoutConstraint!
-//    
+class MovieDetailViewController: UIViewController {
+    
+    var movieDetailView: MovieDetailView!
     var data: Movie!
     
     override func viewDidAppear(animated: Bool) {
@@ -30,43 +119,31 @@ class MovieDetailViewController: UIViewController, UIScrollViewDelegate {
     }
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
+        
+        on("INJECTION_BUNDLE_NOTIFICATION") {
+            self.view = MovieDetailView()
+        }
 
-//        titleLabel.text = data.title
-//        dateLabel.text = data.releaseDate
-//        rateLabel.text = String(data.voteAverage)
-//        
-//        Alamofire.request(.GET, "\(largePoster)\(data.posterUrl)", parameters: ["api_key": apiKey])
-//            
-//           .responseData { (data) in
-//            self.posterImageView.image = UIImage(data: data.data!)
-//        }
-//        
-//        overviewLabel.text = data.overview + data.overview + data.overview
-//        scrollView.delegate = self
-//        contentViewHeight.constant = UIScreen.mainScreen().bounds.height
-//        overviewScrollView.scrollEnabled = false
-//        
-//        MovieDetailCommunicator.getDetailWithMovieId(data.id, successHandler: { (runTime) in
-//            
-//            let h = runTime / 60
-//            let m = runTime % 60
-//            let min = m % 60 == 0 ? "" : "\(m) mins"
-//            self.timeLabel.text = "\(h) hr \(min)"
-//            }) {
-//        }
-    }
-
-    func scrollViewDidScroll(scrollView: UIScrollView) {
-    
-//        if self.scrollView == scrollView {
-//            if scrollView.contentOffset.y  == 0 {
-//                UIView.animateWithDuration(0.5, animations: { 
-//                    self.overviewScrollView.contentOffset.y = 0
-//                })
-//            }
-//            overviewScrollView.scrollEnabled = scrollView.contentOffset.y != 0
-//        }
+        self.view = MovieDetailView()
+        movieDetailView = view as! MovieDetailView
+        
+        movieDetailView.titleLabel.text = data.title
+        movieDetailView.dateLabel.text = data.releaseDate
+        movieDetailView.rateLabel.text = String(data.voteAverage)
+        Alamofire.request(.GET, "\(largePoster)\(data.posterUrl)", parameters: ["api_key": apiKey])
+            
+           .responseData { (data) in
+            self.movieDetailView.posterImageView.image = UIImage(data: data.data!)
+        }
+        movieDetailView.overviewLabel.text = data.overview
+        MovieDetailCommunicator.getDetailWithMovieId(data.id, successHandler: { (runTime) in
+            
+            let h = runTime / 60
+            let m = runTime % 60
+            let min = m % 60 == 0 ? "" : "\(m) mins"
+            self.movieDetailView.timeLabel.text = "\(h) hr \(min)"
+            }) {
+        }
     }
 }
---
+++ main.m
@@ -0,0 +1,19 @@
+//
+//  mian.m
+//  QuickFlicks
+//
+//  Created by Dang Quoc Huy on 7/6/16.
+//  Copyright © 2016 Ky Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// From here to end of file added by Injection Plugin //
+
+#ifdef DEBUG
+static char _inMainFilePath[] = __FILE__;
+static const char *_inIPAddresses[] = {"192.168.1.206", "127.0.0.1", 0};
+
+#define INJECTION_ENABLED
+#import "/tmp/injectionforxcode/BundleInjection.h"
+#endif
